b811da25e3b2305184dad61c9750505c9d0ed6ff
zuzegor2211-maker <zuzegor2211@gmail.com>
2026-04-22 13:18:54 +0300

    закамментиорвадл

--- 2.py
+++ 2.py
@@ -1,37 +1,41 @@
+# Задание №2 - Квадратное уравнение
 
-#Задания №2
 class quadratic_equations:
-    def __init__(self, a,b,c):
-        self.a = a
-        self.b = b
-        self.c = c
+    # Конструктор класса, принимает коэффициенты a, b, c
+    def __init__(self, a, b, c):
+        self.a = a  # коэффициент при x²
+        self.b = b  # коэффициент при x
+        self.c = c  # свободный член
 
-        #Проверка коэффициентов
+    # Проверка коэффициентов (убеждаемся, что уравнение квадратное)
     def checking_coefficient(self):
         if self.a == 0:
             raise ValueError("Коэффициент 'a' не может быть равен 0 (уравнение не квадратное)")
         
-        #Дискременат
+    # Вычисление дискриминанта
     def discriminant(self):
         return self.b**2 - 4*self.a*self.c 
 
-        #Нахождения корней
+    # Нахождение корней уравнения
     def solve(self):
-        D = self.discriminant()
+        D = self.discriminant()  # получаем дискриминант
 
-        if D > 0:
+        if D > 0:  # два корня
             x1 = (-self.b + D**0.5) / (2*self.a)
             x2 = (-self.b - D**0.5) / (2*self.a)
             return x1, x2
-        elif D == 0:
+        elif D == 0:  # один корень
             x = -self.b / (2*self.a)
             return x
-        else:
+        else:  # корней нет
             return None
 
-Ivan = quadratic_equations(1, -3, 2)  # x² - 3x + 2 = 0
+# Создаём объект: уравнение x² - 3x + 2 = 0
+Ivan = quadratic_equations(1, -3, 2)
+
+# Выводим дискриминант
 print(f"Дискриминант: {Ivan.discriminant()}")
+
+# Находим и выводим корни
 roots = Ivan.solve()
 print(f"Корни уравнения: x1 = {roots[0]}, x2 = {roots[1]}")
-
-
